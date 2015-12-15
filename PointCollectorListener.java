8b5800d6ff42ac2223a2ef9a30b881160ca06fd7
Jack Daniels <jakday777@gmail.com>
2015-12-14 20:22:03 -0600

    Create PointCollectorListener.java

---
+++ PointCollectorListener.java
@@ -0,0 +1,10 @@
+package com.danielschmidt.myapplication;
+
+
+import android.graphics.Point;
+
+import java.util.List;
+
+public interface PointCollectorListener {
+    public void pointsCollected(List<Point> points);
+}
